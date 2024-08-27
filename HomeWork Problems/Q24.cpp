f17a99952534e849d99beeb497e41bf3ea4ecc46
Har Aziz Singh <singhharaziz@gmail.com>
2024-08-27 10:10:08 +0530

    Insertion Sort

---
+++ HomeWork Problems/Q24.cpp
@@ -0,0 +1,27 @@
+//Insertion Sort
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[6]={1, 3,2,5, 7, 8};
+
+    int temp;
+    for (int i=1;i<6;i++){
+        temp=arr[i];
+        int j=i-1;
+        for (;j>0;j--){
+            if (arr[j]>temp){
+                arr[j+1]=arr[j];
+            }
+            else{
+                break;
+            }
+   
+        }
+     arr[j+1]=temp;
+    }
+
+    for ( int i=0;i<6;i++){
+        cout<<arr[i]<<" ";
+    }
+}
